4ce6de51623ee3fc65e68571eecb54787444911c
soheejang15 <jsh167528@gmail.com>
2018-07-24 19:55:41 +0900

    Field

---
+++ chap06/Car.java
@@ -0,0 +1,11 @@
+package chap06;
+
+public class Car {
+
+	String company = "현대자동차";
+	String model = "그랜저";
+	String color = "검정";
+	int maxSpeed = 350;
+	int speed;
+
+}
---
+++ chap06/CarExample.java
@@ -0,0 +1,20 @@
+// 외부 클래스에서 필드값 읽기와 변경
+package chap06;
+
+public class CarExample {
+	public static void main(String[] args) {
+		// 객체 생성
+		Car myCar = new Car();
+		
+		//필드값 읽기
+		System.out.println("제작회사 : "+myCar.company);
+		System.out.println("모델명 : "+myCar.model);
+		System.out.println("색깔 : "+myCar.color);
+		System.out.println("최고속도 : "+myCar.maxSpeed);
+		System.out.println("현재속도 : "+myCar.speed);
+		
+		//필드값 변경
+		myCar.speed = 60;
+		System.out.println("수정된 속도 : "+myCar.speed);
+	}
+}
