cee3eaf521bf01431788a29fd72661366a372a5e
soheejang15 <jsh167528@gmail.com>
2018-07-26 12:27:34 +0900

    Final

---
+++ chap06/Earth.java
@@ -0,0 +1,12 @@
+package chap06;
+
+public class Earth {
+
+	static final double EARTH_RADIUS = 6400;
+	static final double EARTH_SURFACE_AREA;
+	
+	static {
+		EARTH_SURFACE_AREA = 4*Math.PI*EARTH_RADIUS*EARTH_RADIUS;
+	}
+
+}
