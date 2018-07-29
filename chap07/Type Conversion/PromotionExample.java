9eb16855eeb95fb6d459a49961515fdc1e5b4bb5
soheejang15 <jsh167528@gmail.com>
2018-07-29 23:47:39 +0900

    Type Conversion

---
+++ chap07/Type Conversion/PromotionExample.java
@@ -0,0 +1,31 @@
+package chap07;
+
+class A{}
+
+class B extends A{}
+class C extends A{}
+
+class D extends B{}
+class E extends C{}
+
+public class PromotionExample {
+
+	public static void main(String[] args) {
+		B b = new B();
+		C c = new C();
+		D d = new D();
+		E e = new E();
+		
+		A a1 = b;
+		A a2 = c;
+		A a3 = d;
+		A a4 = e;
+		
+		B b1 = d;
+		C c1 = e;
+		
+		//B b3 = e; 惑加 包拌 x
+		//C c2 = d; 惑加 包拌 x
+	}
+
+}
